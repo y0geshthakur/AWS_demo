6e8112e1ad2d59c0e0ae8a56300f792b1c8e6f8d
Yogesh Thakur <work.yogeshthakur@gmail.com>
2025-03-10 14:01:37 +0530

    fixed errors

--- task01/app/lambdas/hello_world/index.js
+++ task01/app/lambdas/hello_world/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello from Lambda" }),
+        "message": "Hello from Lambda"
     };
     return response;
 };
